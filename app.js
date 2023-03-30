76c63201d53ffef28a1ce91072df1ce57b2c862f
oleg <olegvraby@gmail.com>
2023-03-30 10:13:00 +0200

    added section WORK

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 
 let items = ['Boy food', 'Cook food', 'Eat food'];
+let workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -22,13 +23,30 @@ app.get('/', (req, res) => {
 
   let day = today.toLocaleDateString('en-US', options);
 
-  res.render('list', { day: day, items: items });
+  res.render('list', { listTitle: day, items: items });
 });
 
 app.post('/', (req, res) => {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect('/');
+  
+  if(req.body.list === 'Work'){
+    workItems.push(item)
+    res.redirect('/work')
+  }else {
+    items.push(item);
+    res.redirect('/');
+  }
+
+});
+
+app.get('/work', (req, res) => {
+  res.render('list', { listTitle: 'Work List', items: workItems });
+});
+
+app.post('/work', (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
 });
 
 app.listen(3000, () => {
